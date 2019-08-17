464d858ab1cb158c6ffb03e9fa6df5aa4566f6d7
pipparichter <philippa.a.richter@gmail.com>
2019-08-17 14:00:37 -0400

    had to clean up dictionaries manually because some characters were weird af

--- categorization/categorizer.py
+++ categorization/categorizer.py
@@ -13,6 +13,7 @@ import re
 #   Music                   Mindblowing
 #   Art and Design          Outdoors
 
+
 def remove_duplicates(lst):
 
     lst = sorted(lst)
@@ -26,10 +27,13 @@ def remove_duplicates(lst):
     return [word for word in lst if word != '']
 
 
+# 'combined' is a dictionary in which each key is a letter of the alphabet, and each value is a list containing
+# every word from both dictionaries which begins with that letter
+combined = {}
+
 # Load and clean dictionaries
 with open("./urban_dict.txt", 'r') as ud, open("./dict.txt", 'r') as d:
     
-    combined = {}
     alph = "abcdefghijklmnopqrstuvwxyz"
     
     ud = ud.read().split('\n')
@@ -39,22 +43,23 @@ with open("./urban_dict.txt", 'r') as ud, open("./dict.txt", 'r') as d:
     d_index = 0
 
     for letter in alph:
-        
+       
         words = []
-        pattern = re.compile(f"^[{letter}{letter.upper()}]")
+        pattern = re.compile(f"^[{letter}{letter.upper()}].*$")
         # Subreddit names can only contain letters, numbers, and underscores, and no more than
         # 21 characters
         subreddit_rules = re.compile("^[\w]{1,21}$")
         
-        while (pattern.match(ud[ud_index])):
+        while pattern.match(ud[ud_index]):
 
-            if (subreddit_rules.match(ud[ud_index])):
+            if subreddit_rules.match(ud[ud_index]):
                 
                 words.append(ud[ud_index].lower())
             
             ud_index += 1
+            print(ud[ud_index])
 
-        while (pattern.match(d[d_index])):
+        while pattern.match(d[d_index]):
 
             if (subreddit_rules.match(d[d_index])):
 
@@ -65,4 +70,4 @@ with open("./urban_dict.txt", 'r') as ud, open("./dict.txt", 'r') as d:
         words = remove_duplicates(words)
         combined[letter] = words
 
-print(combined)
+
